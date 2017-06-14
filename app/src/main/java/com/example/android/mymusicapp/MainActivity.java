2700f3906f505a0d8368d535f07ebc67fc4ed415
ROBIN RAJ <creativerobin04@gmail.com>
2017-06-14 17:07:41 +0530

    Create MainActivity.java

--- app/src/main/java/com/example/android/mymusicapp/MainActivity.java
+++ app/src/main/java/com/example/android/mymusicapp/MainActivity.java
@@ -1,7 +1,10 @@
-package com.example.android.mymusicapp;
+package com.example.android.robmusic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnhome = (Button)findViewById(R.id.btnhome);
+        btnhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inthome = new Intent(MainActivity.this,MyMusicActivity.class);
+                startActivity(inthome);
+            }
+        });
+        Button btndscvr = (Button)findViewById(R.id.discover);
+        btndscvr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inthome = new Intent(MainActivity.this,DiscoverMusic.class);
+                startActivity(inthome);
+            }
+        });
+        Button btncharts = (Button)findViewById(R.id.charts);
+        btncharts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inthome = new Intent(MainActivity.this,TrendingCharts.class);
+                startActivity(inthome);
+            }
+        });
+
     }
 }
