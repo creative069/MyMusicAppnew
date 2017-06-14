e7fad841c103360b85491dba74c0b0578ef15288
ROBIN RAJ <creativerobin04@gmail.com>
2017-06-14 17:33:46 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/android/mymusicapp/TrendingCharts.java
@@ -0,0 +1,13 @@
+package com.example.android.robmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrendingCharts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trending_charts);
+    }
+}
